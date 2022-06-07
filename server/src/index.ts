2ae2168d9a9d097f04a8f8ddf1942d3eb13f8f60
Sqwyer <sawyer.codes.stuff@gmail.com>
2022-06-07 16:32:22 -0500

    Start on frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import * as hbs from 'hbs';
 import { protect } from './middleware/protected';
 import { config } from 'dotenv';
 import { GoogleRouter } from './routes/google';
@@ -17,6 +18,8 @@ const cwd = process.cwd();
 const pInit = passport.initialize.bind(passport);
 const pSession = passport.session.bind(passport);
 
+hbs.registerPartials(`${cwd}/client/partials`);
+
 app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 app.use(pInit());
 app.use(pSession());
@@ -24,6 +27,11 @@ app.set('view engine', 'hbs');
 app.set('views', `${cwd}/client/pages`);
 
 app.use(GoogleRouter);
+
+if(process.env.NODE_ENV != 'production') app.use('/css', express.static(`${cwd}/client/_css`));
+else app.use('/css', express.static(`${cwd}/client/css`));
+
+app.use('/js', express.static(`${cwd}/client/dist`));
 
 app.get('/', protect, (req, res) => {
     res.render('dashboard', {user: req.user});
