9e838d66d01475a97b88275a4dd5ad4cf54bb24d
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2022-07-23 10:46:13 -0500

    Remove unused code

--- server/src/utils/containsAll.ts
+++ server/src/utils/containsAll.ts
@@ -1,9 +1,3 @@
-function arrayContainsAll(a: any[], b: any[]): boolean {
-    return a.every((e) => {
-        return b.includes(e)
-    })
-}
-
 function objectContainsAll(a: any, b: any[]): boolean {
     for (let i = 0; i < b.length; i++) {
         if (!a[b[i]]) return false
@@ -11,4 +5,4 @@ function objectContainsAll(a: any, b: any[]): boolean {
     return true
 }
 
-export { arrayContainsAll, objectContainsAll }
+export { objectContainsAll }
