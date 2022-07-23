2567fb63fb4ba891cf27dfd761f31f35d6645ee6
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2022-07-23 11:32:37 -0500

    🚀 New sidebar

--- server/src/routes/create.ts
+++ server/src/routes/create.ts
@@ -1,10 +1,18 @@
 import { Router, Request, Response } from "express"
 import { protect } from "../middleware/protected"
+import { User } from "../utils/user"
 
 const CreateRouter = Router()
 
-CreateRouter.get("/project", protect, (req: Request, res: Response) => {
-    res.render("create_project", { user: req.user })
+CreateRouter.get("/project", protect, async (req: Request, res: Response) => {
+    await User.getProjects(req.user.id)
+        .then((projects) => {
+            res.render("create_project", { user: req.user, projects })
+        })
+        .catch((err) => {
+            res.render("create_project", { user: req.user, projects: [] })
+            console.error(err)
+        })
 })
 
 export { CreateRouter }
